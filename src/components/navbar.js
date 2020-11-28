e431040db4c38a5856417fb908f1ead3cc577ee6
Eduzor Kamsi <eduzorkamsi@gmail.com>
2020-11-28 21:06:05 +0100

    healthcare04

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,21 +1,36 @@
 import React from "react";
-import "../styles/navbar.css"
+import "../styles/navbar.css";
+import Bell from "../assets/icons/bell.svg";
+import ProfileFace from "../assets/icons/profile.svg";
 
-const Navbar=()=>{
-
-    return(
-        <>
-        <div className="navbar">
-        <div class="topnav">
-
-        <form action="/action_page.php">
-<input type="text" placeholder="Search.."/>
-</form>
-</div>
-
+const Navbar = () => {
+  return (
+    <>
+      <div className="navbar">
+        <div class="leftnav">
+          <form id="search-form">
+            <div class="search">
+              <input
+                type="text"
+                name="search"
+                class="round"
+                placeholder="&#xf002; Search"
+              />
+            </div>
+          </form>
         </div>
-        </>
-    )
-}
+        <div className="rightnav">
+          <div className="bell">
+            <img src={Bell} className="Bell-Icon" alt="Icon" />
+            <span class="badge">9</span>
+          </div>
+          <div className="profile_face">
+            <img src={ProfileFace} className="profile" alt="Icon" />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default Navbar
+export default Navbar;
